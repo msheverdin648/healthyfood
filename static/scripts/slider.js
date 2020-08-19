28e2e21b5d3aa4c3590f44c2a4addfda116b3a2b
msheverdin648 <55691064+msheverdin648@users.noreply.github.com>
2020-08-19 03:04:43 +0300

    create second slider and calc

--- static/scripts/slider.js
+++ static/scripts/slider.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $('.header__slider').slick({
+$('.header__slider').each(function(){
+    $(this).slick({
         dots: true, 
+        arrows: true,
+    });
+ });
+
+ $('.slider__slick').each(function(){
+    $(this).slick({
+        dots: true, 
+        slidesToShow: 5,
+        arrows: true,
+        slidesToScroll: 4,
     });
  });
