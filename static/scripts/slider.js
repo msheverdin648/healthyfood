b9994eee3161e14b369bdb600d08b396043f925e
msheverdin648 <55691064+msheverdin648@users.noreply.github.com>
2020-08-20 00:45:46 +0300

    make menu sliders

--- static/scripts/slider.js
+++ static/scripts/slider.js
@@ -5,11 +5,37 @@ $('.header__slider').each(function(){
     });
  });
 
+
+
  $('.slider__slick').each(function(){
     $(this).slick({
         dots: true, 
         slidesToShow: 5,
         arrows: true,
         slidesToScroll: 4,
+    });
+ });
+
+
+
+ $(document).ready(function(){
+    $('.menu__slider__small').slick({
+
+        dots: false, 
+        slidesToShow: 5,
+        arrows: true,
+        vertical: true,
+        slidesToScroll: 1,
+
+    });
+ });
+
+
+ $(document).ready(function(){
+    $('.menu__slider__big').slick({
+
+        dots: false, 
+        arrows: true,
+
     });
  });
