cd813e46ebed91a1e17c80ade9284315d4a6df47
msheverdin648 <55691064+msheverdin648@users.noreply.github.com>
2020-09-02 00:26:36 +0300

    make big and small blocks templates

--- healthyfood/apps/main_page/views.py
+++ healthyfood/apps/main_page/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import HeaderSlider, PageSlider, PageHeaders
+from .models import HeaderSlider, PageSlider, PageHeaders, ProgrammsBig, ProgrammsSmall
 
 
 def index(request):
@@ -8,9 +8,13 @@ def index(request):
     header_slides = HeaderSlider.objects.all()
     page_slides = PageSlider.objects.all()
     page_headers = PageHeaders.objects.get(id=1)
+    big_block = ProgrammsBig.objects.all()
+    small_block = ProgrammsSmall.objects.all()
     return render(request, 'main_page/page.html', {'header_slides': header_slides,
     'headers': page_headers,
     'page_slides': page_slides,
+    'big_block': big_block,
+    'small_block': small_block,
 
      
 
