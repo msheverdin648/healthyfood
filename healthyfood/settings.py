c0e7a6e2b3cd99b00022908968c40bbd84be4549
msheverdin648 <55691064+msheverdin648@users.noreply.github.com>
2020-09-03 23:06:33 +0300

    q2132

--- healthyfood/settings.py
+++ healthyfood/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(PROJECT_ROOT, 'static')
 STATICFIELS_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
      )
 
 MEDIA_URL = '/media/'
